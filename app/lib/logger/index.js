3ed7dc24bea67efe48492b3232fccc914c2fde13
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2024-10-05 21:29:32 -0500

    enhancing logger with timestamp

--- app/lib/logger/index.js
+++ app/lib/logger/index.js
@@ -1,6 +1,10 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple(),
+    ),
     transports: [ new winston.transports.Console() ],
 });
 
