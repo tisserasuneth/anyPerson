e2f2656c908b8e89f53ef146ffb10d1debc1d03c
tisserasuneth@gmail.com <tisserasuneth@gmail.com>
2024-11-18 13:10:53 -0600

    adding error event

--- app/routes/api/chat.js
+++ app/routes/api/chat.js
@@ -3,6 +3,12 @@ import logger from "../../lib/logger/index.js";
 
 const SOCKET_TIMEOUT = 5 * 60 * 1000;
 
+const OPENAI_EVENTS = {
+    TEXT_DELTA: 'textDelta',
+    TEXT_DONE: 'textDone',
+    ERROR: 'error'
+};
+
 function setTimer(socket) {
     if (socket.timeOutTimer) {
         clearTimeout(socket.timeOutTimer);
@@ -31,7 +37,6 @@ function initializeSocket(socket, controller) {
 }
 
 function handleSocket(webSocket) {
-    try {
         webSocket.on('connection', (socket) => {
             const controller = new Chat();
             initializeSocket(socket, controller);
@@ -42,12 +47,18 @@ function handleSocket(webSocket) {
                     const { message } = event;
                     const stream = await controller.handleMessage(message);
 
-                    stream.on('textDelta', async delta => {
-                        await socket.emit(Chat.EVENTS.SYSTEM_MESSAGE_CHUNK, delta.value);
+                    stream.on(OPENAI_EVENTS.TEXT_DELTA, async delta => {
+                        socket.emit(Chat.EVENTS.SYSTEM_MESSAGE_CHUNK, delta.value);
                     });
 
-                    stream.on('textDone', async delta => {
-                        await socket.emit(Chat.EVENTS.SYSTEM_MESSAGE_END);
+                    stream.on(OPENAI_EVENTS.TEXT_DONE, async delta => {
+                        socket.emit(Chat.EVENTS.SYSTEM_MESSAGE_END);
+                    });
+
+                    stream.on(OPENAI_EVENTS.ERROR, async error => {
+                        const errorMessage = `Error encountered while handling message event: ${error.message || error}`;
+                        logger.error(errorMessage);
+                        socket.emit(Chat.EVENTS.ERROR, `Error encountered while handling message event`);
                     });
 
                 } catch (error) {
@@ -63,10 +74,6 @@ function handleSocket(webSocket) {
                 socket.disconnect(true);
             });
         });
-    } catch (error) {
-        const errorMessage = `Error handling socket events: ${error.message || error}`;
-        logger.error(errorMessage);
-    }
 }
 
 export default handleSocket;
